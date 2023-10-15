351e24418b326a351dd1687359c3b5f613141396
amirQuf <amir.ghasemian.ag@gmail.com>
2023-10-15 13:39:54 +0330

    code review part1 Server class

--- core/server.py
+++ core/server.py
@@ -4,12 +4,12 @@ from socket import *
 
 clients = {}  # client information
 users = {}  # client sockets for broadcast
-threads = [] # list of threads
-usernames = [] # list of usernames
+threads = []  # list of threads
+usernames = []  # list of usernames
 
 
 # broadcast function
-def broadcast(m):
+def broadcast(m: str):
     # iterate Clients and sending message for everyone
     for client in clients.values():
         client.send(m)
@@ -17,13 +17,13 @@ def broadcast(m):
 
 # this function is for receiving message and answer to them
 def client_send(clientSocket, addr):
-    request = b''
+    request = b""
     while request != b"Bye.":
         # receiving messages from clients
         request = clientSocket.recv(1024)
         # decode message
         request.decode()
-        if request[0:5] == b'Hello':
+        if request[0:5] == b"Hello":
             # print('hello')
             # finding username
             username = request[6:]
@@ -35,7 +35,7 @@ def client_send(clientSocket, addr):
                 print(welcome_str)
                 # send welcome message to user
                 clientSocket.send(welcome_str)
-                # send noftication message for other users
+                # send notification message for other users
                 notification_str = username + b" join the chat room."
                 print(notification_str)
                 broadcast(notification_str)
@@ -43,10 +43,10 @@ def client_send(clientSocket, addr):
                 users[username] = clientSocket
             else:
                 # sending error message to user because username was taken
-                er_str = b'''username  is exist please try again!'''
+                er_str = b"""username  is exist please try again!"""
                 print(er_str)
                 clientSocket.send(er_str)
-        elif request == b'Bye':
+        elif request == b"Bye":
             # print('bye')
             # sending a message  for other users  that this user left chat room
             msg = username + b" left the chat room."
@@ -54,22 +54,20 @@ def client_send(clientSocket, addr):
             broadcast(msg)
             # remove username from list
             usernames.remove(username)
-            break;
-        elif request == b'Please send the list of attendees.':
-            # print('list')
+            break
+        elif request == b"Please send the list of attendees.":
             # sending list of attendees
-            response = b'Here is the list of attendees:\n'
+            response = b"Here is the list of attendees:\n"
             # iterate all usernames and make the string and send to user
             for user in usernames:
-                response = response + user + b','
+                response = response + user + b","
             print(response[:-1])
             clientSocket.send(response)
-        elif request[0:6] == b'Public':
-            # print('public')
+        elif request[0:6] == b"Public":
             # getting information about public message
-            public_str = request[:13] + b' from ' + username + request[13:]
+            public_str = request[:13] + b" from " + username + request[13:]
             # finding and return length in message with regex and concat it to int
-            length = int(re.findall(r'\d+', str(request))[0])
+            length = int(re.findall(r"\d+", str(request))[0])
             # receiving  message body
             public_msg = clientSocket.recv(length)
             # print the message
@@ -78,15 +76,14 @@ def client_send(clientSocket, addr):
             public_response = str(public_str) + "\r\n" + str(public_msg)
             print(username, ":", public_response)
             broadcast(public_response.encode())
-        elif request[0:7] == b'Private':
-
+        elif request[0:7] == b"Private":
             # print('private')
-            pr_msg = b'''Private message from ''' + username
+            pr_msg = b"""Private message from """ + username
 
             print(pr_msg)
             # getting information about public message
             # finding length with regex and concat to int
-            length = int(re.findall(r'\d+', str(request))[0])
+            length = int(re.findall(r"\d+", str(request))[0])
             # receive message body and decode it
             private_msg = clientSocket.recv(length).decode()
             pr_response = str(pr_msg) + "\r\n" + private_msg
@@ -100,38 +97,53 @@ def client_send(clientSocket, addr):
                     users[u].send(pr_response_bytes)
         else:
             # SENDING AND PRINTING ERROR FOR UNAVAILABLE MESSAGE
-            er_strr = 'undefined command'
+            er_strr = "undefined command"
             print(er_strr)
             clientSocket.send(er_strr.encode())
         # print("done")
 
+class ServerException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
 
-SERVER_NAME = ""  # server name(local host)
-SERVER_PORT = 21000  # server port
+class Server:
+    def __init__(self, server_name: str, server_port: int) -> None:
+        self.server_name = server_name
+        self.server_port = server_port
 
-# creating and binding Tcp socket for server
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind((SERVER_NAME, SERVER_PORT))
-print('Server started!')
-print('Waiting for clients...')
-print('Got connection from', SERVER_NAME, SERVER_PORT)
-# listening  to  users
-server_socket.listen(100)
+    def binding_port(self):
+        """binding port for server to listen to clients"""
+        self._server_socket = socket(AF_INET, SOCK_STREAM)
+        self._server_socket.bind((self.server_name, self.server_socket))
+        print("Server started!")
+        print("Waiting for clients...")
+        print("Got connection from", SERVER_NAME, SERVER_PORT)
+        self._server_socket.listen(100)
 
-while 1:
-    # a client connect  to  server and we should accept them for the rest
-    connection_Socket, addr = server_socket.accept()
-    # saving connection Socket
-    clients[connection_Socket.fileno()] = connection_Socket
-    # creating thread for answering to multiple clients
-    t = threading.Thread(target=client_send, args=(connection_Socket, addr))
-    # start the thread
-    t.start()
-# closing the sever socket
-server_socket.close()
+    def listen_to_clients(self):
+        '''listening to clients'''
+        while 1:
+        # a client connect  to  server and we should accept them for the rest
+        connection_socket, addr = self._server_socket.accept()
+        # saving connection Socket
+        clients[connection_socket.fileno()] = connection_socket
+        # creating thread for answering to multiple clients
+        t = threading.Thread(target=client_send, args=(connection_socket, addr))
+        # start the thread
+        t.start()
+        # closing the sever socket
+        self._server_socket.close()
 
 
 
 
-#end  of file 
-
+if __name__=="__main__":
+    SERVER_NAME = ""  # server name(local host)
+    SERVER_PORT = 21000  # server port
+    
+    server = Server(SERVER_NAME,SERVER_PORT)
+    try:
+        server.binding_port()
+    except:
+        raise ServerException(detail=f"{SERVER_NAME}:{SERVER_PORT}Port is Busy")
+    server.listen_to_clients()
